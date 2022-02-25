4000cd986606bbdcb08ab012766fe35c23e0a2de
sasan sahami <sasan_2002@yahoo.com>
2022-02-26 00:48:24 +1100

    change server and add routes

--- server.js
+++ server.js
@@ -1,89 +1,28 @@
+// Dependencies
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
 
-// Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
+// Point Server to the route files
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = 3001;
-
+// Create an express server
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
+// Set PORT
+const PORT = process.env.PORT || 3001;
+
+// Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 
+// Parse incoming JSON data
+app.use(express.json());
+
+
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-// Promise version of fs.readFile
-const readFromFile = util.promisify(fs.readFile);
-
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
-
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
-const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
-};
-
-// GET Route for retrieving all the notes
-app.get('/api/notes', (req, res) => {
-  console.info(`${req.method} request received for notes`);
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+// Listener
+app.listen(PORT, () => {
+    console.log(`API server is ready on port ${PORT}!`);
 });
-
-// POST Route for a new note
-app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-
-  const { title, text } = req.body;
-
-  if (req.body) {
-    const newNote = {
-      title,
-      text,
-      id: uuid(),
-    };
-
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully 🚀`);
-  } else {
-    res.error('Error in adding note');
-  }
-});
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
